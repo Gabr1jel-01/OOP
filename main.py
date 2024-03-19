f4c6e08565fc549adb423282e5df7f3acc2c5f7e
Gabr1jel-01 <76105935+Gabr1jel-01@users.noreply.github.com>
2024-03-19 21:03:47 +0100

    a

--- main.py
+++ main.py
@@ -1,22 +1,54 @@
+import csv
+
 class Item:
+    pay_rate = 0.8 # The pay rate after after 20% discount
+    all = []
     #init je konstruktor metoda koja se pokrece automatski kod kreiranja instance klase
-    def __init__(self,name,price,quantity=0):   #kada stavim nulu pod atribut metode to mi dozvoljava da ne moram ispuniti taj atribut pri stvaranju instance klase
+    def __init__(self,name: str,price: float, quantity=0):   #kada stavim nulu pod atribut metode to mi dozvoljava da ne moram ispuniti taj atribut pri stvaranju instance klase
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
+        
+        # Assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
         
+        # Actions to execute
+        Item.all.append(self)
         
-    def calculate_total_price(self):
+    def calculate_total_price(self): # U ovu metodu ne trebam stavljati parametre osim self zato sto mi self omogucava da pristupim atributima u konstruktor init metodi
         return self.price * self.quantity
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate       
         
+    def __repr__(self):
+        return f"Item('{self.name}',{self.price},{self.quantity})"
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv','r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
         
+        for item in items:
+            Item(
+                name = item.get('name'),
+                price = float(item.get('price')),
+                quantity = int(item.get('quantity'))
+            )
+    
+    @staticmethod        
+    def is_integer(num):
+        # We will count out the floats that are point zero
+        # For i.e; 5.0, 2.0
+        if isinstance(num, float):
+            # Count out the floats that are point zero
+            return num.is_integer()
         
-        
-        
-        
-item1 = Item("Phone", 100, 4)
+Item.instantiate_from_csv()
+print(Item.all)
 
 
-item1.has_numpad = False
 
-print(item1.has_numpad) #ovom linijom sam dodao jos jedan atribut u metodu __init__. To sto nije dodana gore pri kreiranju ne znaci da ne mogu nekada poslije dodati
