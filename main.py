6278bb8fe35b24bed76af4665fe890aab1a43b48
Gabr1jel-01 <76105935+Gabr1jel-01@users.noreply.github.com>
2024-03-20 19:56:11 +0100

    a

--- main.py
+++ main.py
@@ -46,9 +46,23 @@ class Item:
         if isinstance(num, float):
             # Count out the floats that are point zero
             return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            pass
+
+
+class Phone(Item):
+    pass
+
+
+
+
+
         
-Item.instantiate_from_csv()
-print(Item.all)
+phone1 = Phone("jscPhonev10", 500, 5)
+phone1.broken_phones = 1
 
-
+phone2 = Phone("jscPhonev20", 700, 5)
+phone2.broken_phones = 1
 
